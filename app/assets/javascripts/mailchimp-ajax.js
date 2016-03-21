568e9117221a48ac08bd8b30909ba580c6ef253f
John Hampton <john_j_hampton@yahoo.com>
2016-03-20 23:19:19 -0400

    Add signupUser promise function that returns AJAX response - issue #21
    Conditionally display different success or error messages based on Mailchimp AJAX response

--- app/assets/javascripts/mailchimp-ajax.js
+++ app/assets/javascripts/mailchimp-ajax.js
@@ -1,7 +1,9 @@
+// credit: https://github.com/rydama/mailchimp-ajax-signup, http://stackoverflow.com/a/15120409/215821
+
 (function() {
 
   var $form = $('#mailchimp-form');
-  var $notification = $('.contact-box__notification');
+  var $notification = $('#mailchimp-notification');
 
   $form.submit(function(event) {
     event.preventDefault();
@@ -14,18 +16,46 @@
       $notification.css("color", "red");
     }
     else {
-      $notification.html(success);
-      $notification.show();
-      $notification.css("color", "inherit");
+      signupUser($form, $notification).then(function() {
+        $notification.show();
+      });
     }
-  })
+  });
 
   function isValidEmail($form) {
     var inputText = $form.find("input[type='email']").val();
-    console.log(inputText);
     if (!inputText || !inputText.length) {
       return false;
     }
     return true;
+  }
+
+  function signupUser($form, $notification) {
+    return $.ajax({
+        type: $form.attr('method'),
+        url: $form.attr('action'),
+        data: $form.serialize(),
+        cache: false,
+        dataType: 'jsonp',
+        contentType: "application/json; charset=utf-8",
+        error: function(err) {
+          // not needed for JSONP cross-domain requests
+        },
+        success: function(data) {
+          var message;
+          if (data.result != "success") {
+              message = data.msg || "Sorry. Unable to subscribe. Please try again later.";
+              $notification.css("color", "red");
+              if (data.msg && data.msg.indexOf("already subscribed") >= 0) {
+                message = "You're already subscribed. Thank you.";
+                $notification.css("color", "inherit");
+              }
+              $notification.html(message);
+          } else {
+            $notification.css("color", "inherit");
+            $notification.html("Thanks for signing up! Please check your email to confirm.");
+          }
+        }
+    });
   }
 })()
