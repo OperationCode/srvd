c084142845ba642fdb1db0923cbe57a7be7338a1
John Hampton <john_j_hampton@yahoo.com>
2016-03-21 00:43:53 -0400

    Clean up AJAX promise success and error handlers
    Conditionally remove button & input when successful signup

--- app/assets/javascripts/mailchimp-ajax.js
+++ app/assets/javascripts/mailchimp-ajax.js
@@ -4,27 +4,44 @@
 
   var $form = $('#mailchimp-form');
   var $notification = $('#mailchimp-notification');
+  var $inputEl = $form.find("input[type='email']");
 
   $form.submit(function(event) {
+    var $inputText = $inputEl.val();
     event.preventDefault();
-    var error = "Please enter a valid email.";
-    var success = "Thanks for signing up! Please check your email for confirmation.";
+    var message = "Please enter a valid email.";
 
-    if (!isValidEmail($form)) {
-      $notification.html(error);
-      $notification.show();
+    if (!isValidEmail($inputText)) {
+      $notification.html(message);
       $notification.css("color", "red");
+      $notification.show();
     }
     else {
-      signupUser($form, $notification).then(function() {
+      signupUser($form, $notification).then(function(data) {
+        if (data.result === "success") {
+          $notification.css("color", "inherit");
+          $('.contact-box__button').css('display', "none");
+          $inputEl.css('display', "none");
+          message = "Thanks for signing up! Please check your email to confirm.";
+        }
+        else if (data.result === "error"){
+          if (data.msg && data.msg.indexOf("already subscribed") >= 0) {
+            message = "You're already subscribed. Thank you.";
+            $notification.css("color", "inherit");
+          }
+          else {
+            message = "Sorry. Unable to subscribe. Please try again later.";
+            $notification.css("color", "red");
+          }
+        }
+        $notification.html(message);
         $notification.show();
       });
     }
   });
 
-  function isValidEmail($form) {
-    var inputText = $form.find("input[type='email']").val();
-    if (!inputText || !inputText.length) {
+  function isValidEmail($inputText) {
+    if (!$inputText || !$inputText.length) {
       return false;
     }
     return true;
@@ -38,24 +55,6 @@
         cache: false,
         dataType: 'jsonp',
         contentType: "application/json; charset=utf-8",
-        error: function(err) {
-          // not needed for JSONP cross-domain requests
-        },
-        success: function(data) {
-          var message;
-          if (data.result != "success") {
-              message = data.msg || "Sorry. Unable to subscribe. Please try again later.";
-              $notification.css("color", "red");
-              if (data.msg && data.msg.indexOf("already subscribed") >= 0) {
-                message = "You're already subscribed. Thank you.";
-                $notification.css("color", "inherit");
-              }
-              $notification.html(message);
-          } else {
-            $notification.css("color", "inherit");
-            $notification.html("Thanks for signing up! Please check your email to confirm.");
-          }
-        }
     });
   }
 })()
